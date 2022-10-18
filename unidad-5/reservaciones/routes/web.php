2b5b1eb792d500b286ea45e0f8ad0b8930b52904
Abdiel Flores <abdielfg@gmail.com>
2022-10-18 09:59:31 -0600

    Martes 18 OCT 2022 rutas get laravel

--- unidad-5/reservaciones/routes/web.php
+++ unidad-5/reservaciones/routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
+use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -30,6 +32,19 @@ Route::get('suma/{num1}/{num2}/{num3?}',function($num1,$num2,$num3=0){
     echo $num1 + $num2 + $num3;
 })->where(['num1'=>'[0-9]+','num2'=>'[0-9]+']);
 
-Route::post('suma/',function(Request $request){
 
-});
+Route::get('users/', [UserController::class,'index']);
+
+Route::get('users/create',[UserController::class, 'create']);
+
+Route::get('users/{id}',[UserController::class,'show']);
+
+Route::post('users/', [UserController::class,'store']);
+
+
+
+
+
+// Route::post('suma/',function(Request $request){
+
+// });
